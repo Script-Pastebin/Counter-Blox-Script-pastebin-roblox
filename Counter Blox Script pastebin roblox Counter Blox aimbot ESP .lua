e79b42c5d25785674bce189334751d8c195b9251
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 00:33:18 +0100

    commit zero

---
+++ Counter Blox Script pastebin roblox Counter Blox aimbot ESP .lua
@@ -0,0 +1,12 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							aim the bot to turn on nada, press E and also turn on nada on E
+to turn on ESP nada press T
+						
+]]
